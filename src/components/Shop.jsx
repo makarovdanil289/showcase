8113f2132cd7c4ea6f62fb5df922b1ce23bc3ae0
Danil <makarovdanil289@gmail.com>
2024-07-02 22:35:15 +0500

    project complete

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,11 +4,15 @@ import { API_KEY, API_URL } from "../config";
 import { Preloader } from "./Prealoader";
 import { GoodsList } from "./GoodsList";
 import { Cart } from "./Cart";
+import { BasketList } from "./BasketList";
+import { Alert } from "./Alert";
 
 function Shop() {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
     const [order, SetOrder] = useState([]);
+    const [isBasketShow, setBasketShow] = useState(false);
+    const [alertName, setAlertName] = useState("");
 
     const addToBasket = (item) => {
         const itemIndex = order.findIndex(
@@ -35,6 +39,50 @@ function Shop() {
 
             SetOrder(newOrder);
         }
+        setAlertName(item.name);
+    };
+
+    const removeFromBasket = (itemId) => {
+        const newOrder = order.filter((el) => el.id !== itemId);
+        SetOrder(newOrder);
+    };
+
+    const incQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity + 1;
+                return {
+                    ...el,
+                    quantity: newQuantity,
+                };
+            } else {
+                return el;
+            }
+        });
+        SetOrder(newOrder);
+    };
+
+    const decQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity - 1;
+                return {
+                    ...el,
+                    quantity: newQuantity >= 0 ? newQuantity : 0,
+                };
+            } else {
+                return el;
+            }
+        });
+        SetOrder(newOrder);
+    };
+
+    const handleBasketShow = () => {
+        setBasketShow(!isBasketShow);
+    };
+
+    const closeAlert = () => {
+        setAlertName("");
     };
 
     useEffect(function getGoods() {
@@ -52,12 +100,22 @@ function Shop() {
 
     return (
         <main className="container content">
-            <Cart quantity={order.length} />
+            <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
             {loading ? (
                 <Preloader />
             ) : (
                 <GoodsList goods={goods} addToBasket={addToBasket} />
             )}
+            {isBasketShow && (
+                <BasketList
+                    order={order}
+                    handleBasketShow={handleBasketShow}
+                    removeFromBasket={removeFromBasket}
+                    incQuantity={incQuantity}
+                    decQuantity={decQuantity}
+                />
+            )}
+            {alertName && <Alert name={alertName} closeAlert={closeAlert} />}
         </main>
     );
 }
